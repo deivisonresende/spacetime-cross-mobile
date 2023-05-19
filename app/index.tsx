18f41e2fc0c9ce565139da7421c38fdb79dcd92e
deivison resende <deivison.resende@enacom.com.br>
2023-05-19 20:23:37 -0300

    feat: added authentication with github OAuth

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,66 @@
+import * as SecureStore from 'expo-secure-store'
+
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import Logo from './src/assets/logo.svg'
+import Logo from '../src/assets/logo.svg'
 import { StatusBar } from 'expo-status-bar'
-import Stripes from './src/assets/stripes.svg'
-import blurBg from './src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import { api } from '../src/lib/api'
+import blurBg from '../src/assets/bg-blur.png'
+import { GITHUB_CLIENT_ID as gitHubClientId } from '@env'
 import { styled } from 'nativewind'
+import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
 
 const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [, response, gitHubSignIn] = useAuthRequest(
+    {
+      clientId: `${gitHubClientId}`,
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    {
+      authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+      tokenEndpoint: 'https://github.com/login/oauth/access_token',
+      revocationEndpoint: `https://github.com/settings/connections/applications/${gitHubClientId}`,
+    },
+  )
+
+  async function handleGhOAuthResponse(response) {
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      const token = await api
+        .post('/register/gh', { code })
+        .then(({ data }) => data.token)
+
+      await SecureStore.setItemAsync('token', token)
+
+      router.push('/memories/register')
+    }
+  }
+  useEffect(() => {
+    handleGhOAuthResponse(response)
+  }, [response])
 
   if (!hasLoadedFonts) return null
 
@@ -45,6 +86,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => gitHubSignIn()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             COMEÇAR A CADASTRAR
